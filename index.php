f8948643edb8d459cd1dd5fc3db68f83a841b320
Henrique Martins <hmartins224@gmail.com>
2020-10-14 10:10:04 -0300

    developing the login page design

--- index.php
+++ index.php
@@ -7,14 +7,30 @@
   <link rel="stylesheet" href="index.css">
 </head>
 <body>
-  <div class="headerImage">
-    <img src="src/assets/logoilustrativa.png" alt="logo">
-  </div>
-  <div class="content">
-    <h1>Login</h1>
-    <input type="text">
-    <input type="text">
-    <button>Entrar</button>
+  <div class="container">
+    <div class="headerImage">
+      <img src="src/assets/logoilustrativa.png" alt="logo">
+    </div>
+    <div class="content">
+      <h1>Login</h1>
+      <div class="input-group">
+				<label class="input-underlined">
+				  <input required>
+				  <span class="input-label">Usuário</span>
+        </label>
+      </div>
+      <div class="input-group">
+				<label class="input-underlined">
+				  <input required>
+				  <span class="input-label">Senha</span>
+        </label>
+      </div>
+      <div class="button-group">
+        <button>
+          Entrar
+        </button>
+      </div>
+    </div>
   </div>
 </body>
 </html>
