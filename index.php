269274672159956029fc6796497e40d7e7d75bc7
Henrique Martins <hmartins224@gmail.com>
2020-10-14 09:17:50 -0300

    Adding the necessary elements on the screen

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
-  echo 'hello rodolfo and claudio';
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>The Control</title>
+  <link rel="stylesheet" href="vars.css">
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+  <div class="headerImage">
+    <img src="src/assets/logoilustrativa.png" alt="logo">
+  </div>
+  <div class="content">
+    <h1>Login</h1>
+    <input type="text">
+    <input type="text">
+    <button>Entrar</button>
+  </div>
+</body>
+</html>
