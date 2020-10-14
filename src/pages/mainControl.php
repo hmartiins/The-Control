2c0150de25b44b13132e72ba5ac26f545847f5dd
João Victor <jv782063@gmail.com>
2020-10-14 10:13:02 -0300

    feat: add more color variables and start the main control screen

---
+++ src/pages/mainControl.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css "
+    />
+    <link rel="stylesheet" href="../../vars.css" />
+    <link rel="stylesheet" href="../styles/mainControl.css" />
+    <title>The Control</title>
+  </head>
+
+  <body>
+      <header>
+        <div class="report">
+          <i class="fa fa-chart-line"></i>
+          <b>Relatórios</b>
+        </div>
+
+        <img src="../assets/logoilustrativa.png" alt="The Control" />
+
+        <div class="logout">
+          <i class="fas fa-sign-out-alt"></i>
+        </div>
+      </header>
+  </body>
+</html>
