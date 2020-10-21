588acf00457ef262483f0f986478c8afee243a28
João Victor <jv782063@gmail.com>
2020-10-21 10:13:57 -0300

    creates the managemente page table and adjusts things that were missing

--- src/pages/management.php
+++ src/pages/management.php
@@ -5,10 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../../vars.css" />
     <link rel="stylesheet" href="../styles/management.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
     <title>The Control</title>
   </head>
@@ -32,7 +28,7 @@
     </header>
 
     <div class="container">
-      <table class="table table-dark mt-5">
+      <table class="table">
         <thead>
           <tr>
             <th scope="col">Nome</th>
